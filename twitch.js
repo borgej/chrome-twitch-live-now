60ed00f9016c18d4a17ba6468db35ed5963ccfb8
Børge Jakobsen <borge.jakobsen@gmail.com>
2020-07-21 18:03:41 +0200

    * Changed manifest.json * Added js files

---
+++ twitch.js
@@ -0,0 +1,21 @@
+var user;
+var followedChannels = [];
+var onlineChannels = [];
+var updateInterval = 5;
+
+document.getElementById("update").addEventListener("click", alertMe);
+
+// TODO: GET user data from input
+function getUser() {
+
+}
+
+// TODO: GET all followed channels of the username
+function getFollowedChannels() {
+
+}
+
+// TODO: Update channels table in the popup.html
+function updateOnlineChannels() {
+
+}
